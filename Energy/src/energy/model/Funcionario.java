5a8028947a7d49cd0ae05670d23da2c77cd4cdd2
Stanley Nhampossa <Nhampossastanley@gmail.com>
2022-05-01 03:57:56 +0200

    Adicionou-se métodos para cadastrar e listar Funcionario de acordo com email

--- Energy/src/energy/model/Funcionario.java
+++ Energy/src/energy/model/Funcionario.java
@@ -1,17 +1,34 @@
 package energy.model;
 
+import energy.dao.ExceptionDAO;
+import energy.dao.FuncionarioDAO;
+
 import java.util.Date;
 
 public class Funcionario extends Pessoa{
 
-    private int numeroDeFuncionario;
-    private AreaDeServicoAoCliente localDeTrabalho;
-    private String categoria;
+    protected int numeroDeFuncionario;
+    protected AreaDeServicoAoCliente localDeTrabalho;
+    protected String categoria;
+    protected EquipasDeTrabalho equipaDeTrabalho;
+    protected int contactoAlternativo;
 
-    public Funcionario(String nome, String apelido, Date dataDeNascimento, String estadoCivil, String nacionalidade, String morada, String email, String numeroDeBI, int contacto, int nuit, String genero, AreaDeServicoAoCliente localDeTrabalho, String categoria) {
+    public Funcionario(String nome, String apelido, Date dataDeNascimento, String estadoCivil, String nacionalidade, String morada, String email, String numeroDeBI, int contacto, int contactoAlternativo, int nuit, String genero, AreaDeServicoAoCliente localDeTrabalho, String categoria) {
         super(nome, apelido, dataDeNascimento, estadoCivil, nacionalidade, morada, email, numeroDeBI, contacto, nuit, genero);
         this.localDeTrabalho = localDeTrabalho;
         this.categoria = categoria;
+        this.contactoAlternativo = contactoAlternativo;
+
+    }
+
+    public Funcionario(){}
+
+    public EquipasDeTrabalho getEquipaDeTrabalho() {
+        return equipaDeTrabalho;
+    }
+
+    public void setEquipaDeTrabalho(EquipasDeTrabalho equipaDeTrabalho) {
+        this.equipaDeTrabalho = equipaDeTrabalho;
     }
 
     public String getCategoria() {
@@ -30,12 +47,28 @@ public class Funcionario extends Pessoa{
         this.numeroDeFuncionario = numeroDeFuncionario;
     }
 
+    public int getContactoAlternativo() {
+        return contactoAlternativo;
+    }
+
+    public void setContactoAlternativo(int contactoAlternativo) {
+        this.contactoAlternativo = contactoAlternativo;
+    }
+
     public AreaDeServicoAoCliente getLocalDeTrabalho() {
         return localDeTrabalho;
     }
 
     public void setLocalDeTrabalho(AreaDeServicoAoCliente localDeTrabalho) {
         this.localDeTrabalho = localDeTrabalho;
+    }
+
+    public void cadastarFuncionario(Funcionario funcionario) throws ExceptionDAO {
+        new FuncionarioDAO().cadastrarFuncionario(funcionario);
+    }
+
+    public Funcionario encontrarFuncionarioPorEmail(String email)throws ExceptionDAO{
+        return new FuncionarioDAO().encontrarFuncionarioPorEmail(email);
     }
 
 }
