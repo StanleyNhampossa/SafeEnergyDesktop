9e1d78a83c3f8c95154155d94d725e024123992f
Stanley Nhampossa <Nhampossastanley@gmail.com>
2022-05-01 03:57:56 +0200

    Adicionou-se métodos para cadastrar e listar Funcionario de acordo com email

---
+++ Energy/src/energy/controller/FuncionarioController.java
@@ -0,0 +1,95 @@
+package energy.controller;
+
+import energy.dao.ExceptionDAO;
+import energy.model.AreaDeServicoAoCliente;
+import energy.model.Funcionario;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FuncionarioController {
+
+    public boolean cadastrarFuncionario(String nome, String apelido, String dataDeNascimento, String estadoCivil, String profissao, String morada, String email,
+                                        String numeroDeBI, int contacto, int contactoAlternativo, int nuit, String genero, AreaDeServicoAoCliente areaDeServicoAoCliente, String categoria) throws ParseException, ExceptionDAO {
+
+        if( (nome != null && nome.length() > 0) && (apelido != null && apelido.length() > 0) && dataDeNascimento.length() > 0 && validarDataDeNascimento(dataDeNascimento) && (estadoCivil != null && estadoCivil.length() > 0) &&
+                (profissao != null & profissao.length() > 0) && (morada != null && morada.length() > 0) && (email.length() > 0 && validarEmail(email)) && (numeroDeBI.length() > 0 && validarNoBI(numeroDeBI))
+                &&  (String.valueOf(contacto).length() > 0 && validarContacto(contacto)) && String.valueOf(contactoAlternativo).length() > 0 && validarContacto(contactoAlternativo) && String.valueOf(nuit).length() > 0 && validarNUIT(nuit) && validarSexo(genero) && (categoria != null && categoria.length() > 0)) {
+
+            //Ele faz a formatação da data de nascimento
+            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/YYYY");
+            Date data = formato.parse(dataDeNascimento);
+
+            //Ele chama o método a classe da Model para poder cadastrar o funcionário
+            Funcionario funcionario = new Funcionario(nome, apelido, data, estadoCivil, profissao, morada, email, numeroDeBI, contacto, contactoAlternativo, nuit, genero, areaDeServicoAoCliente, categoria);
+            funcionario.setPalavraPasse(apelido + nuit);
+            funcionario.cadastarFuncionario(funcionario);
+
+            return true;
+
+        }
+        return false;
+    }
+
+    public Funcionario encontrarFuncionario(String email) throws ExceptionDAO{
+        return new Funcionario().encontrarFuncionarioPorEmail(email);
+    }
+
+
+    public boolean validarSexo(String sexo){
+        if(sexo == "M" || sexo == "F"){
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    public boolean validarDataDeNascimento(String dataDeNascimento){
+        for(int i = 0; i < dataDeNascimento.length(); i ++){
+            if(!(Character.isDigit(dataDeNascimento.charAt(i)))){
+                if(!(i == 2 || i == 5) ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean validarEmail(String email){
+        if(email.matches("^[A-Za-z0-9+.-]+@(.+)$")){
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public boolean validarNoBI(String noBI){
+        for(int i = 0; i < noBI.length(); i ++){
+            if(!(Character.isDigit(noBI.charAt(i)))){
+                if(!(i == 13)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean validarContacto(int contacto){
+        //if(contacto.matches("[82-87]+[0-9]*{5}"))
+
+
+        return true;
+
+    }
+
+
+    public boolean validarNUIT(int nuit){
+        return true;
+    }
+
+
+}
---
+++ Energy/src/energy/dao/FuncionarioDAO.java
@@ -0,0 +1,135 @@
+package energy.dao;
+
+import energy.model.AreaDeServicoAoCliente;
+import energy.model.Funcionario;
+
+import java.sql.*;
+
+public class FuncionarioDAO {
+
+    public void cadastrarFuncionario(Funcionario funcionario) throws ExceptionDAO{
+        String querySQL = "insert into funcionario (codigoDeASC, categoria, nome, apelido, " +
+                "palavraPasse, " +
+                "dataDeNascimento, estadoCivil, profissao, morada, email, " +
+                "numeroDeBI, contacto, contactoAlternativo, nuit, genero) value (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement preparedStatement = null;
+        Connection conexao = null;
+
+        try {
+            conexao = new ConnectionMVC().getConnection();
+            preparedStatement = conexao.prepareStatement(querySQL);
+
+            preparedStatement.setInt(1, funcionario.getLocalDeTrabalho().getCodigoDeASC());
+            preparedStatement.setString(2, funcionario.getCategoria());
+            preparedStatement.setString(3, funcionario.getNome());
+            preparedStatement.setString(4, funcionario.getApelido());
+            preparedStatement.setString(5, funcionario.getPalavraPasse());
+            preparedStatement.setDate(6, new Date(funcionario.getDataDeNascimento().getTime()));
+            preparedStatement.setString(7, funcionario.getEstadoCivil());
+            preparedStatement.setString(8, funcionario.getProfissao());
+            preparedStatement.setString(9, funcionario.getMorada());
+            preparedStatement.setString(10, funcionario.getEmail());
+            preparedStatement.setString(11, funcionario.getNumeroDeBI());
+            preparedStatement.setInt(12, funcionario.getContacto());
+            preparedStatement.setInt(13, funcionario.getContactoAlternativo());
+            preparedStatement.setInt(14, funcionario.getNuit());
+            preparedStatement.setString(15, funcionario.getGenero());
+
+            preparedStatement.execute();
+
+        }catch (SQLException e){
+            throw new ExceptionDAO("Erro ao cadastrar funcionário " + e);
+        }finally {
+
+            try {
+                if(preparedStatement != null)
+                    preparedStatement.close();
+            }catch (SQLException e){
+                throw new ExceptionDAO("Erro ao fechar statement " + e);
+            }
+
+            try {
+                if(conexao != null)
+                    conexao.close();
+            }catch (SQLException e){
+                throw new ExceptionDAO("Erro ao fechar conexão " + e);
+            }
+
+        }
+
+    }
+
+    public Funcionario encontrarFuncionarioPorEmail(String email) throws ExceptionDAO{
+
+        String querySQL = "select * from funcionario where email = ?";
+
+        Connection conexao = null;
+        PreparedStatement preparedStatement = null;
+
+        Funcionario funcionario = null;
+
+        try {
+            conexao = new ConnectionMVC().getConnection();
+            preparedStatement = conexao.prepareStatement(querySQL);
+
+            preparedStatement.setString(1, email);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                funcionario = new Funcionario();
+
+                funcionario.setNumeroDeFuncionario(resultSet.getInt(1));
+                //funcionario.setLocalDeTrabalho((AreaDeServicoAoCliente) resultSet.getObject(2));
+                //funcionario.setEquipaDeTrabalho();
+                funcionario.setCategoria(resultSet.getString(4));
+                funcionario.setNome(resultSet.getString(5));
+                funcionario.setApelido(resultSet.getString(6));
+                funcionario.setPalavraPasse(resultSet.getString(7));
+                funcionario.setDataDeNascimento(resultSet.getDate(8));
+                funcionario.setEstadoCivil(resultSet.getString(9));
+                funcionario.setProfissao(resultSet.getString(10));
+                funcionario.setMorada(resultSet.getString(11));
+                funcionario.setEmail(resultSet.getString(12));
+                funcionario.setNumeroDeBI(resultSet.getString(13));
+                funcionario.setContacto(resultSet.getInt(14));
+                funcionario.setContactoAlternativo(resultSet.getInt(15));
+                funcionario.setNuit(resultSet.getInt(16));
+                funcionario.setGenero(resultSet.getString(17));
+
+                /*
+                numeroDeFuncionario int auto_increment not null,
+                codigoDeASC int,
+                numeroDaEquipa int,
+
+                 */
+
+
+            }
+
+
+        }catch (SQLException e){
+            throw new ExceptionDAO("Erro ao consultar funcionário " + e);
+        }finally {
+            try {
+                if(conexao != null)
+                    conexao.close();
+
+            }catch (SQLException e){
+                throw new ExceptionDAO("Erro ao fechar a conexão " + e);
+            }
+
+            try {
+                if(preparedStatement != null)
+                    preparedStatement.close();
+
+            }catch (SQLException e){
+                throw new ExceptionDAO("Erro ao fechar o statement " + e);
+            }
+        }
+
+        return funcionario;
+    }
+
+}
